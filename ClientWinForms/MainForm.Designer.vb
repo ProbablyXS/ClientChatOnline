aeb7e2cd31f0e2d78f71ff833c5e06f65d2d7bc0
Probably <vecrproject@hotmail.com>
2022-02-22 21:23:12 +0100

    Initial commit

---
+++ ClientWinForms/MainForm.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ButtonConnect = New System.Windows.Forms.Button()
+        Me.MessageTextBox = New System.Windows.Forms.TextBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.ButtonSend = New System.Windows.Forms.Button()
+        Me.UsernameTextBox = New System.Windows.Forms.TextBox()
+        Me.IpTextBox = New System.Windows.Forms.TextBox()
+        Me.FontDialog1 = New System.Windows.Forms.FontDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'ButtonConnect
+        '
+        Me.ButtonConnect.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.ButtonConnect.Location = New System.Drawing.Point(132, 58)
+        Me.ButtonConnect.Name = "ButtonConnect"
+        Me.ButtonConnect.Size = New System.Drawing.Size(100, 23)
+        Me.ButtonConnect.TabIndex = 0
+        Me.ButtonConnect.Text = "Connect"
+        Me.ButtonConnect.UseVisualStyleBackColor = True
+        '
+        'MessageTextBox
+        '
+        Me.MessageTextBox.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.MessageTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.MessageTextBox.Location = New System.Drawing.Point(12, 282)
+        Me.MessageTextBox.MaxLength = 300
+        Me.MessageTextBox.Multiline = True
+        Me.MessageTextBox.Name = "MessageTextBox"
+        Me.MessageTextBox.Size = New System.Drawing.Size(298, 55)
+        Me.MessageTextBox.TabIndex = 1
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 82)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.Size = New System.Drawing.Size(352, 194)
+        Me.RichTextBox1.TabIndex = 2
+        Me.RichTextBox1.Text = ""
+        '
+        'ButtonSend
+        '
+        Me.ButtonSend.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ButtonSend.Location = New System.Drawing.Point(316, 282)
+        Me.ButtonSend.Name = "ButtonSend"
+        Me.ButtonSend.Size = New System.Drawing.Size(56, 55)
+        Me.ButtonSend.TabIndex = 3
+        Me.ButtonSend.Text = "Send"
+        Me.ButtonSend.UseVisualStyleBackColor = True
+        '
+        'UsernameTextBox
+        '
+        Me.UsernameTextBox.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom), System.Windows.Forms.AnchorStyles)
+        Me.UsernameTextBox.ForeColor = System.Drawing.Color.Black
+        Me.UsernameTextBox.Location = New System.Drawing.Point(132, 5)
+        Me.UsernameTextBox.Name = "UsernameTextBox"
+        Me.UsernameTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.UsernameTextBox.TabIndex = 4
+        Me.UsernameTextBox.Text = "XsplitS"
+        '
+        'IpTextBox
+        '
+        Me.IpTextBox.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom), System.Windows.Forms.AnchorStyles)
+        Me.IpTextBox.Location = New System.Drawing.Point(132, 32)
+        Me.IpTextBox.Name = "IpTextBox"
+        Me.IpTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.IpTextBox.TabIndex = 5
+        Me.IpTextBox.Text = "127.0.0.1"
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Button1.Location = New System.Drawing.Point(302, 5)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(70, 40)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "FONT SIZE"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(27, 64)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(99, 17)
+        Me.CheckBox1.TabIndex = 7
+        Me.CheckBox1.Text = "Auto reconnect"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(376, 349)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.IpTextBox)
+        Me.Controls.Add(Me.UsernameTextBox)
+        Me.Controls.Add(Me.ButtonSend)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.MessageTextBox)
+        Me.Controls.Add(Me.ButtonConnect)
+        Me.DoubleBuffered = True
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "MainForm"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonConnect As Button
+    Friend WithEvents MessageTextBox As TextBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents ButtonSend As Button
+    Friend WithEvents UsernameTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents IpTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents FontDialog1 As FontDialog
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+End Class
---
+++ ClientWinForms/MainForm.vb
@@ -0,0 +1,168 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+
+Public Class MainForm
+
+    Dim connected As Boolean = False
+    Dim timer As Integer = 3 ' = 5 secondes
+    Dim _client As New TcpClient
+
+    Public Async Function ConnectToTheServer() As Task
+
+        If connected = True Then
+            Return
+        End If
+
+        connected = True
+
+        Await Task.Delay(1)
+
+        Try
+
+            Dim ip As String = IpTextBox.Text
+            Dim port As Integer = 5656
+
+            _client = New TcpClient
+            Await _client.ConnectAsync(ip, port)
+
+            CheckForIllegalCrossThreadCalls = False
+
+            Threading.ThreadPool.QueueUserWorkItem(AddressOf ReceiveMessages) 'Start ReceReceiveMessages function
+
+
+            'SEND CLIENT CONNECTION MESSAGE
+            Dim Clientconnected As String = UsernameTextBox.Text
+            Dim ns As NetworkStream = _client.GetStream()
+
+            ns.Write(Encoding.UTF8.GetBytes(Clientconnected), 0, Clientconnected.Length)
+
+            ButtonConnect.Enabled = False
+            'END SEND CLIENT CONNECTION MESSAGE
+
+        Catch ex As Exception
+
+            RichTextBox1.Text = "Cannot connect from the server..."
+
+            ButtonConnect.Enabled = True
+            connected = False
+            AutoReconnect()
+            Return
+
+        End Try
+
+    End Function
+
+    Public Sub SendMessage(Username As String, Message As String)
+
+        If connected = False Then
+            Return
+        End If
+
+        Try
+            Dim ns As NetworkStream = _client.GetStream()
+            Dim text As String = Username & " : " & Message
+            ns.Write(Encoding.UTF8.GetBytes(text), 0, text.Length)
+            MessageTextBox.Clear()
+
+        Catch ex As Exception
+            RichTextBox1.Text = "Cannot receive information from the server..."
+            ButtonConnect.Enabled = True
+            connected = False
+            AutoReconnect()
+        End Try
+
+
+    End Sub
+
+    Private Async Sub ReceiveMessages(state As Object)
+
+
+        Try
+            While True
+
+                Dim ns As NetworkStream = _client.GetStream()
+                Dim toReceive(100000) As Byte
+                ns.Read(toReceive, 0, toReceive.Length)
+                Dim txt As String = Encoding.UTF8.GetString(toReceive)
+
+                Await Task.Delay(100)
+                If RichTextBox1.TextLength > 0 Then
+                    RichTextBox1.Text &= vbNewLine & txt
+                Else
+                    RichTextBox1.Text = txt
+                End If
+
+                'SCROLL TO THE END
+                RichTextBox1.SelectionStart = RichTextBox1.Text.Length
+                RichTextBox1.ScrollToCaret()
+                'END SCROLL TO THE END
+
+            End While
+        Catch ex As Exception
+
+            RichTextBox1.Text = "Server closed..."
+            ButtonConnect.Enabled = True
+            connected = False
+            AutoReconnect()
+
+        End Try
+
+
+    End Sub
+
+
+
+    Private Sub MessageTextBox_KeyDown(sender As Object, e As KeyEventArgs) Handles MessageTextBox.KeyDown
+
+        If (e.KeyCode = Keys.Enter) Then
+            e.SuppressKeyPress = True
+            SendMessage(UsernameTextBox.Text, MessageTextBox.Text)
+        End If
+
+    End Sub
+
+    Private Sub ButtonSend_Click(sender As Object, e As EventArgs) Handles ButtonSend.Click
+        SendMessage(UsernameTextBox.Text, MessageTextBox.Text)
+    End Sub
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim hostName As String = Dns.GetHostName()
+        IpTextBox.Text = Dns.GetHostByName(hostName).AddressList(0).ToString()
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        FontDialog1.ShowDialog()
+        RichTextBox1.Font = FontDialog1.Font
+        MessageTextBox.Font = FontDialog1.Font
+    End Sub
+
+    Public Async Sub AutoReconnect()
+
+        If CheckBox1.Checked = False Or connected = True Then
+            Return
+        Else
+            connected = True
+        End If
+
+        While True
+            Await Task.Delay(1)
+            If timer = 0 Then
+                timer = 3 ' = 5 secondes
+                RichTextBox1.Text = "Connecting"
+                connected = False
+                Await ConnectToTheServer()
+                Return
+            Else
+                RichTextBox1.Text = "Trying to reconnect from the server: " & timer
+                timer -= 1
+                Await Task.Delay(1000)
+            End If
+        End While
+
+    End Sub
+
+    Private Async Sub ButtonConnect_Click(sender As Object, e As EventArgs) Handles ButtonConnect.Click
+        Await ConnectToTheServer()
+    End Sub
+End Class
